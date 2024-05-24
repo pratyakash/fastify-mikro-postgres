358f7ed8e1f327937d42998b5bb2647415eaa66b
Pratyakash Saini <pratyakash13@gmail.com>
2024-05-24 16:37:33 +0530

    fix: port

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,11 @@ import server from './app';
 
 const start = async () => {
   try {
+    const PORT = Number(process.env.PORT) ?? 3000;
+
     await server.ready();
-    await server.listen({ port: 3000 });
-    console.log(`Server is running at http://localhost:3000`);
+    await server.listen({ port: PORT });
+    console.log(`Server is running at http://localhost:${PORT}`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
